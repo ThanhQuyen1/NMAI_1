97da9da52cd50c56aba074451b3bf02406b8f9c8
ThanhQuyen1 <21130508@st.hcmuaf.edu.vn>
2023-10-01 00:19:06 +0700

    add

---
+++ task2/AgentProgram.java
@@ -0,0 +1,15 @@
+package task2;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		if (p.getLocationState() == p.getLocationState().DIRTY) {
+			return Environment.SUCK_DIRT;
+		} else if(p.getAgentLocation() == Environment.LOCATION_A) {
+			return Environment.MOVE_RIGHT;
+		}else {
+			return Environment.MOVE_LEFT;
+		}
+		
+	}
+}
